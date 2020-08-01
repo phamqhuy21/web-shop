8feb6e9e2a5c765046f20aaf0e4cde91227d2ceb
phamqhuy21 <phamqhuy21.@gmail.com>
2020-08-01 10:15:44 +0700

    Updates

---
+++ precache-manifest.657c9c8379efebef145857d8c20f0961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f23e93e290a050a0e9b2201cb1b977",
+    "url": "/web-shop/index.html"
+  },
+  {
+    "revision": "75de7a669bb3b6f045aa",
+    "url": "/web-shop/static/css/main.12767a1a.chunk.css"
+  },
+  {
+    "revision": "09e9b61f27bf6b158d6c",
+    "url": "/web-shop/static/js/2.5c8067d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-shop/static/js/2.5c8067d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75de7a669bb3b6f045aa",
+    "url": "/web-shop/static/js/main.a6a70e17.chunk.js"
+  },
+  {
+    "revision": "9d00eaf551d6c825eafd",
+    "url": "/web-shop/static/js/runtime-main.1010f173.js"
+  },
+  {
+    "revision": "d6059139872d004a5f16ea2cde779098",
+    "url": "/web-shop/static/media/adam.d6059139.PNG"
+  }
+]);
