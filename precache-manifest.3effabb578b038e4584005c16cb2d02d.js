6681709fbebdb282f1325a611996eec9c5701b19
phamqhuy21 <phamqhuy21.@gmail.com>
2020-08-01 10:51:54 +0700

    Updates

--- precache-manifest.3effabb578b038e4584005c16cb2d02d.js
+++ precache-manifest.3effabb578b038e4584005c16cb2d02d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f539626954465d6f1fd615934300e07",
+    "revision": "190675ac132235a164df707e87b78017",
     "url": "/web-shop/index.html"
   },
   {
-    "revision": "e51b339d07a84a855d4f",
-    "url": "/web-shop/static/css/main.0e537a9f.chunk.css"
+    "revision": "7c78051c4d7c2a593c8e",
+    "url": "/web-shop/static/css/main.a424bde9.chunk.css"
   },
   {
     "revision": "e0461c887926e81f1df7",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/web-shop/static/js/2.ee21b734.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e51b339d07a84a855d4f",
+    "revision": "7c78051c4d7c2a593c8e",
     "url": "/web-shop/static/js/main.53ac4c78.chunk.js"
   },
   {
