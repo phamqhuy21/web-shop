3a4c0ebc5ef0912a5310a86b2ffdfdce39e39dfd
phamqhuy21 <phamqhuy21.@gmail.com>
2020-08-02 09:01:52 +0700

    Updates

---
+++ precache-manifest.2889002ee2cecd942863b26d0f522c89.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b17bea34f7439d319b5471e79c1e337",
+    "url": "/web-shop/index.html"
+  },
+  {
+    "revision": "0dbb8aa1bfd695855f0b",
+    "url": "/web-shop/static/css/main.8f5a225c.chunk.css"
+  },
+  {
+    "revision": "37e4d3df7da07d524696",
+    "url": "/web-shop/static/js/2.1873113a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-shop/static/js/2.1873113a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbb8aa1bfd695855f0b",
+    "url": "/web-shop/static/js/main.592ee099.chunk.js"
+  },
+  {
+    "revision": "9d00eaf551d6c825eafd",
+    "url": "/web-shop/static/js/runtime-main.1010f173.js"
+  },
+  {
+    "revision": "4f0fd29695dee01dd80e47b21a24548d",
+    "url": "/web-shop/static/media/adam.4f0fd296.PNG"
+  },
+  {
+    "revision": "d6059139872d004a5f16ea2cde779098",
+    "url": "/web-shop/static/media/adam.d6059139.PNG"
+  },
+  {
+    "revision": "423b679d203aed80331e477327d89dbd",
+    "url": "/web-shop/static/media/adam7.423b679d.PNG"
+  },
+  {
+    "revision": "09c375d7596dbce7bc04d11631c7c9af",
+    "url": "/web-shop/static/media/adam8.09c375d7.PNG"
+  },
+  {
+    "revision": "afee69f1f069f9ae120bc0d587d9c06f",
+    "url": "/web-shop/static/media/giay1.afee69f1.PNG"
+  },
+  {
+    "revision": "a6a06912c4c0d2470f8d1394f516d6fc",
+    "url": "/web-shop/static/media/giay10.a6a06912.PNG"
+  },
+  {
+    "revision": "927807bf196ac72f67a5717ece79a7e6",
+    "url": "/web-shop/static/media/giay11.927807bf.PNG"
+  },
+  {
+    "revision": "7a09305f5b3d2c1f2738c1857f2e8a30",
+    "url": "/web-shop/static/media/giay12.7a09305f.PNG"
+  },
+  {
+    "revision": "56d4cec56c960c218534218e7fdd0179",
+    "url": "/web-shop/static/media/giay2.56d4cec5.PNG"
+  },
+  {
+    "revision": "ca0f16b8c7b88f79e52ab713f4937828",
+    "url": "/web-shop/static/media/giay3.ca0f16b8.PNG"
+  },
+  {
+    "revision": "1cea4c09e64bcd77b7808ad05e059b29",
+    "url": "/web-shop/static/media/giay4.1cea4c09.PNG"
+  },
+  {
+    "revision": "b036f323a1c6fb5e0d16a66375a328f3",
+    "url": "/web-shop/static/media/giay5.b036f323.PNG"
+  },
+  {
+    "revision": "1e3aabbd89df5ee2c800ab33ee79131b",
+    "url": "/web-shop/static/media/giay6.1e3aabbd.PNG"
+  },
+  {
+    "revision": "1394f7d01f29e195f8eceac992947105",
+    "url": "/web-shop/static/media/giay7.1394f7d0.PNG"
+  },
+  {
+    "revision": "e3e91a5fb3ba3204bdcec3488946ee43",
+    "url": "/web-shop/static/media/giay8.e3e91a5f.PNG"
+  },
+  {
+    "revision": "e5e2f071f45c1d855d385ad46f5d7f74",
+    "url": "/web-shop/static/media/giay9.e5e2f071.PNG"
+  },
+  {
+    "revision": "dc188c16b0cb6a1be8d155cc5cefc2d8",
+    "url": "/web-shop/static/media/phong1.dc188c16.PNG"
+  },
+  {
+    "revision": "4b52c126e3b35e051c4400de7afebe9d",
+    "url": "/web-shop/static/media/phong10.4b52c126.PNG"
+  },
+  {
+    "revision": "8c50d45e946d06f0516ddb78cb3b8398",
+    "url": "/web-shop/static/media/phong11.8c50d45e.PNG"
+  },
+  {
+    "revision": "d361c4ba8c7a10cd9a59973de7f356c1",
+    "url": "/web-shop/static/media/phong12.d361c4ba.PNG"
+  },
+  {
+    "revision": "7233f7520a3500bdf155c2a4463f7a62",
+    "url": "/web-shop/static/media/phong2.7233f752.PNG"
+  },
+  {
+    "revision": "ffc7c55d2b1399322f8d663afaec2a45",
+    "url": "/web-shop/static/media/phong3.ffc7c55d.PNG"
+  },
+  {
+    "revision": "060d8d0ab6d8ab378ccd0ef12e04801f",
+    "url": "/web-shop/static/media/phong4.060d8d0a.PNG"
+  },
+  {
+    "revision": "ba06205d7012f970ff939ccc9c3d922f",
+    "url": "/web-shop/static/media/phong5.ba06205d.PNG"
+  },
+  {
+    "revision": "3e05083d325e78459c56300e0ea12d99",
+    "url": "/web-shop/static/media/phong6.3e05083d.PNG"
+  },
+  {
+    "revision": "1c22a2beac1e17395ac69fb74a97bf8b",
+    "url": "/web-shop/static/media/phong7.1c22a2be.PNG"
+  },
+  {
+    "revision": "7f600aadbecbf660378e7677a9e0fcc3",
+    "url": "/web-shop/static/media/phong8.7f600aad.PNG"
+  },
+  {
+    "revision": "35a311121f6b865662ff6a0cd2d8c7df",
+    "url": "/web-shop/static/media/phong9.35a31112.PNG"
+  },
+  {
+    "revision": "3196caf9617c35b52b69540a09edfe62",
+    "url": "/web-shop/static/media/quan1.3196caf9.PNG"
+  },
+  {
+    "revision": "71c3ffb0c43a66651cdb2922c0e0b74e",
+    "url": "/web-shop/static/media/quan10.71c3ffb0.PNG"
+  },
+  {
+    "revision": "980acfe757d7a556af5afddaa70fe518",
+    "url": "/web-shop/static/media/quan11.980acfe7.PNG"
+  },
+  {
+    "revision": "f239d6da588356372620b26013c869c8",
+    "url": "/web-shop/static/media/quan12.f239d6da.PNG"
+  },
+  {
+    "revision": "a0241ccd13d821e3432b7d243c163d22",
+    "url": "/web-shop/static/media/quan2.a0241ccd.PNG"
+  },
+  {
+    "revision": "0a8102d0be3084458bd92073cfbb9392",
+    "url": "/web-shop/static/media/quan3.0a8102d0.PNG"
+  },
+  {
+    "revision": "d34c8628a8e546a85f1b70ac894731f5",
+    "url": "/web-shop/static/media/quan4.d34c8628.PNG"
+  },
+  {
+    "revision": "2d6576740e59181fd50c927f819f2085",
+    "url": "/web-shop/static/media/quan5.2d657674.PNG"
+  },
+  {
+    "revision": "f1e80ab8acbcbb5da502a3722461da6f",
+    "url": "/web-shop/static/media/quan6.f1e80ab8.PNG"
+  },
+  {
+    "revision": "4c41bc8777c18a64e4d888f36d68095c",
+    "url": "/web-shop/static/media/quan7.4c41bc87.PNG"
+  },
+  {
+    "revision": "335087126dc80114a2b9970d896e1834",
+    "url": "/web-shop/static/media/quan8.33508712.PNG"
+  },
+  {
+    "revision": "4a749003a46e4d5a167dde20d18dd2f7",
+    "url": "/web-shop/static/media/quan9.4a749003.PNG"
+  },
+  {
+    "revision": "2ede2c4877c0c4b0519e93594ca960af",
+    "url": "/web-shop/static/media/somi1.2ede2c48.PNG"
+  },
+  {
+    "revision": "a234f7797b5680d2ab0336d2d16ef074",
+    "url": "/web-shop/static/media/somi10.a234f779.PNG"
+  },
+  {
+    "revision": "9d0329d0675d103d4619394481f53cc2",
+    "url": "/web-shop/static/media/somi11.9d0329d0.PNG"
+  },
+  {
+    "revision": "86ebec0bc66b78e8663e40c75efc9a8b",
+    "url": "/web-shop/static/media/somi12.86ebec0b.PNG"
+  },
+  {
+    "revision": "5f741f9a5c111eaad331a7b2bc35e052",
+    "url": "/web-shop/static/media/somi2.5f741f9a.PNG"
+  },
+  {
+    "revision": "f721a2bb84a40735c2ef8e5dc5fa1b78",
+    "url": "/web-shop/static/media/somi3.f721a2bb.PNG"
+  },
+  {
+    "revision": "9db2fbe1074489aaa1ccd8a3dba814a3",
+    "url": "/web-shop/static/media/somi4.9db2fbe1.PNG"
+  },
+  {
+    "revision": "cae132a303cc4fefcd32be7691c20f5c",
+    "url": "/web-shop/static/media/somi5.cae132a3.PNG"
+  },
+  {
+    "revision": "8542b4bb1bd8b909d3884c5b70cdc7e0",
+    "url": "/web-shop/static/media/somi6.8542b4bb.PNG"
+  },
+  {
+    "revision": "529d184b342922bc0e2278292c644693",
+    "url": "/web-shop/static/media/somi7.529d184b.PNG"
+  },
+  {
+    "revision": "d4e3529b6a943e21b422e5ebbbfe251d",
+    "url": "/web-shop/static/media/somi8.d4e3529b.PNG"
+  },
+  {
+    "revision": "3e6bef3ea2b32c8d3674e1ae0c9aefe5",
+    "url": "/web-shop/static/media/somi9.3e6bef3e.PNG"
+  },
+  {
+    "revision": "9d8bef370d5da760a89651332b76bc2d",
+    "url": "/web-shop/static/media/vest1.9d8bef37.PNG"
+  },
+  {
+    "revision": "8ad490520ba9a7e87e5d7525b128fd75",
+    "url": "/web-shop/static/media/vest10.8ad49052.PNG"
+  },
+  {
+    "revision": "372a5ffb9359aac4339e6f99f6d72a32",
+    "url": "/web-shop/static/media/vest11.372a5ffb.PNG"
+  },
+  {
+    "revision": "90a26b40f57ec0fa4a97919f9a829d10",
+    "url": "/web-shop/static/media/vest12.90a26b40.PNG"
+  },
+  {
+    "revision": "fc2d3f4c13a5ed49eebd56e9ef59a3c6",
+    "url": "/web-shop/static/media/vest2.fc2d3f4c.PNG"
+  },
+  {
+    "revision": "a8ca53e5e2e55002525df32f9d74b2f8",
+    "url": "/web-shop/static/media/vest3.a8ca53e5.PNG"
+  },
+  {
+    "revision": "74e8934c7ebece6716202ab761edf5ae",
+    "url": "/web-shop/static/media/vest4.74e8934c.PNG"
+  },
+  {
+    "revision": "74d116a9fbbb3c59d3e1cc7e2ce5000a",
+    "url": "/web-shop/static/media/vest5.74d116a9.PNG"
+  },
+  {
+    "revision": "0819332d5c0ec93661c6ae31c84e2fec",
+    "url": "/web-shop/static/media/vest6.0819332d.PNG"
+  },
+  {
+    "revision": "772884cb3b9294923d55bb8f2dc6c1b8",
+    "url": "/web-shop/static/media/vest7.772884cb.PNG"
+  },
+  {
+    "revision": "5b73a70b6a599ba0e240e1b13ae429d1",
+    "url": "/web-shop/static/media/vest8.5b73a70b.PNG"
+  },
+  {
+    "revision": "d187676b59e5d282b7a8de5e011353ed",
+    "url": "/web-shop/static/media/vest9.d187676b.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-shop/precache-manifest.b21cb767e38acddfd53d5e00a4c1948e.js"
+  "/web-shop/precache-manifest.2889002ee2cecd942863b26d0f522c89.js"
 );
 
 self.addEventListener('message', (event) => {
